90194cf8fced8dac990529a840ee4c9b317fd3fa
Neciu Dan <dan@adoreme.com>
2020-10-05 20:39:20 +0300

    Removed crm

--- crm/include.user.php
+++
@@ -1,4 +0,0 @@
-                    <div class="dropdown profile-element"> 
-						<!--h4>Academia Testarii CRM</h4-->
-						<span><a href="dashboard.php"><img alt="image" class="img-circle profile-pic" src="img/logo-nou.png" /></a></span>
-                    </div>
